c81c492fb5293d431f5793080b0a144d3862e67b
Jsolivan <42556552+Jsolivan@users.noreply.github.com>
2018-08-27 15:08:31 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,11 +1,25 @@
+/**
+ * ASCII ART
+ * @author Jonathan Solivan CMPSC221
+ */
+
+
+
+
+
 public class Main 
 {
   
    public static void main(String[] args) 
    {
-      System.out.println("  *  ");
-      System.out.println(" *** ");
-      System.out.println("*****");
+      System.out.println("        ___       ");
+      System.out.println("    ___/   \\___   ");
+      System.out.println("   /   \\___/   \\  ");
+      System.out.println("   \\___/   \\___/  ");
+      System.out.println("   /   \\___/   \\  ");
+      System.out.println("   \\___/   \\___/  ");
+      System.out.println("       \\___/      ");
+      
   }
   
 }
